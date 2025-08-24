af9ceed340fc655007e769989c7101d146cac553
Santiagolxx <santiagolxxowo@proton.me>
2025-08-24 02:09:07 -0400

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,22 +6,5 @@ async fn main() {
     tracing_subscriber::fmt().init();
     tracing::info!("Logger iniciado");
 
-    let app_state = cubic_launcher_lib::core::ApplicationState::with_rpc_client(
-        cubic_launcher_lib::core::setup_rpc().await.unwrap(),
-    );
-    {
-        let mut state_guard = app_state.write().await;
-        match state_guard.transition_to_playing("1.21.3").await {
-            Ok(_) => println!("Transición exitosa a jugando"),
-            Err(e) => eprintln!("Error en transición: {}", e),
-        };
-        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
-        match state_guard.transition_to_playing("1.12.2").await {
-            Ok(_) => println!("Transición exitosa a jugando"),
-            Err(e) => eprintln!("Error en transición: {}", e),
-        };
-    }
-    tokio::spawn(async {
-        cubic_launcher_lib::run().await;
-    });
+    cubic_launcher_lib::run().await;
 }
