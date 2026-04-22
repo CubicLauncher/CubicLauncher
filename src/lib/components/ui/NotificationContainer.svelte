1caddf41d61a1f2ec403babbd069f1588e24b652
staFF6773 <nosenivel3@gmail.com>
2026-04-21 21:07:02 -0600

    Organizacion del css

--- src/lib/components/ui/NotificationContainer.svelte
+++ src/lib/components/ui/NotificationContainer.svelte
@@ -11,21 +11,3 @@
         </div>
     {/each}
 </div>
-
-<style>
-    .notification-container {
-        position: fixed;
-        top: 24px;
-        right: 24px;
-        z-index: 9999;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-        pointer-events: none;
-    }
-
-    /* Enable pointer events for the toasts themselves */
-    .notification-container :global(.notification-toast) {
-        pointer-events: auto;
-    }
-</style>
